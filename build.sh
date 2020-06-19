03e763b74c2054b9f7b1c39720ab9e07bf066a07
MMV <mmv.rus@gmail.com>
2020-06-19 15:36:21 +0300

    Add: automatic voipdp-server package download

--- build.sh
+++ build.sh
@@ -1,1 +1,5 @@
+pushd "$(dirname "$0")"
+wget http://download.komendant.pro/voip-dp/linux/voipdp-server_deb_amd64.tgz
+tar --extract -f voipdp-server_deb_amd64.tgz voipdp-server+deb_amd64.deb
 docker build -t mmvrus/voipdp-server:3.5.25-26-debian8 .
+popd
