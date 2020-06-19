62321eb3925c62feab48b6f2617dd8a9efd6a16c
MMV <mmv.rus@gmail.com>
2020-06-19 20:03:56 +0300

    Add: lock to SHA1 for specific version
    failed build preferred to silent update

--- build.sh
+++ build.sh
@@ -1,6 +1,13 @@
-#!/usr/bin/sh
+#!/bin/bash
+export VERSION=3.5.25-26
+export PLATFORM=deb_amd64
 pushd "$(dirname "$0")"
-wget http://download.komendant.pro/voip-dp/linux/voipdp-server_deb_amd64.tgz
-tar --extract -f voipdp-server_deb_amd64.tgz voipdp-server+deb_amd64.deb
-docker build -t mmvrus/voipdp-server:3.5.25-26-debian8 .
+curl -o voipdp-server_deb_amd64.tgz \
+     http://download.komendant.pro/voip-dp/linux/voipdp-server_$PLATFORM.tgz
+if ! sha1sum -c voipdp-server_$(VERSION)_$(PLATFORM).sha1; then
+    echo downloaded package SHA1 not match version $(VERSION)_$(PLATFORM)
+    exit 1
+fi
+tar --extract -f voipdp-server_deb_amd64.tgz voipdp-server+$PLATFORM.deb
+docker build -t mmvrus/voipdp-server:$(VERSION)-debian8 .
 popd
