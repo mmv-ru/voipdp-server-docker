dfc80e3805003aaffa869c082ba436811d9f00e4
MMV <mmv.rus@gmail.com>
2020-06-19 16:42:03 +0300

    Change: use local image name

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ docker run --net=host --name=voipdp-server \
   -v /etc/voipdp-server:/usr/local/voipdp-server/conf:z \
   -v /var/log/voipdp-server:/usr/local/voipdp-server/log:z \
   --restart unless-stopped \
-  mmvrus/voipdp-server:3.5.25-26-debian8
+  voipdp-server:3.5.25-26-debian8
