ceb797fddf3654ff1d0f13351b856e7ede7f235a
root <root@rsos1.sondacha.lan>
2020-06-19 05:23:27 +0300

    Add: build and run shell script for memories

---
+++ build.sh
@@ -0,0 +1,1 @@
+docker build -t mmvrus/voipdp-server:3.5.25-26-debian8 .
---
+++ run.sh
@@ -0,0 +1,1 @@
+docker run --net=host -v /etc/voipdp-server:/usr/local/voipdp-server/conf:z -v /var/log/voipdp-server:/usr/local/voipdp-server/log:z mmvrus/voipdp-server:3.5.25-26-debian8
