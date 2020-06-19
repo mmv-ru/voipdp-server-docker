eaf1f7aaff32f138b2f83eeef18bdc0a6e5094c9
root <root@rsos1.sondacha.lan>
2020-06-19 05:44:25 +0300

    Change: better run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,7 @@
-docker run --net=host -v /etc/voipdp-server:/usr/local/voipdp-server/conf:z -v /var/log/voipdp-server:/usr/local/voipdp-server/log:z mmvrus/voipdp-server:3.5.25-26-debian8
+docker container rm voipdp-server
+docker run --net=host --name=voipdp-server \
+  --detach \
+  -v /etc/voipdp-server:/usr/local/voipdp-server/conf:z \
+  -v /var/log/voipdp-server:/usr/local/voipdp-server/log:z \
+  --restart unless-stopped \
+  mmvrus/voipdp-server:3.5.25-26-debian8
