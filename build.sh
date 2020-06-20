48d18753fb0300bcd08378104920ce2d51e85b14
MMV <mmv.rus@gmail.com>
2020-06-20 17:28:24 +0300

    Fix: variable reference use {}

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@ PLATFORM=deb_amd64
 pushd "$(dirname "$0")"
 curl -o voipdp-server_deb_amd64.tgz \
      http://download.komendant.pro/voip-dp/linux/voipdp-server_$PLATFORM.tgz
-if ! sha1sum -c voipdp-server_$(VERSION)_$(PLATFORM).sha1; then
-    echo downloaded package SHA1 not match version $(VERSION)_$(PLATFORM)
+if ! sha1sum -c voipdp-server_${VERSION}_${PLATFORM}.sha1; then
+    echo downloaded package SHA1 not match voipdp-server version ${VERSION}_${PLATFORM}
     exit 1
 fi
 tar --extract -f voipdp-server_deb_amd64.tgz voipdp-server+$PLATFORM.deb
