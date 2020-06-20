cedf6f198e2e7dcc1893dcc36024b2c7c60c22ea
MMV <mmv.rus@gmail.com>
2020-06-20 17:29:21 +0300

    Add: git metadate to image and parameters to build

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,22 @@
+ARG BASE_IMAGE_TAG=jessie-slim
+
+ARG GIT_ORIGIN
+ARG GIT_DESCRIBE
+ARG GIT_BRANCH
+ARG GIT_UNCOMMITED
+
 #FROM debian:jessie-20200607-slim
-FROM debian:jessie-slim
+FROM debian:$BASE_IMAGE_TAG
 
 ARG VOIPDP_SERVER-ARCHIVE=voipdp-server_deb_amd64.tgz
 ARG VOIPDP_PACKAGE=voipdp-server+deb_amd64.deb
 
-LABEL maintainer="Mikhail Moskalev mmv.rus@gmail.com" \
-      git_repository="https://github.com/mmv-ru/voipdp-server-docker.git"
+LABEL MAINTAINER="Mikhail Moskalev mmv.rus@gmail.com" \
+      GITgit_repository="https://github.com/mmv-ru/voipdp-server-docker.git" \
+      GIT_DESCRIBE="$GIT_DESCRIBE" \
+      GIT_BRANCH="$GIT_BRANCH" \
+      GIT_UNCOMMITED="$GIT_UNCOMMITED" \
+      GIT_ORIGIN="$GIT_ORIGIN"
 
 COPY $VOIPDP_PACKAGE /package/voipdp-server_amd64.deb
 RUN apt-get update && dpkg -i /package/voipdp-server_amd64.deb || apt-get -fy install
--- build.sh
+++ build.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 VERSION=3.5.25-26
 PLATFORM=deb_amd64
+DOCKER_HUB_LOGIN=mmvrus
+
 pushd "$(dirname "$0")"
+
+GIT_ORIGIN=$(git remote get-url origin)
+GIT_DESCRIBE=$(git describe --tag)
+GIT_BRANCH="$(git symbolic-ref HEAD 2>/dev/null)" ||
+  GIT_BRANCH="(unnamed branch)"     # detached HEAD
+GIT_BRANCH=${GIT_BRANCH##refs/heads/}
+# Check uncommited changes
+GIT_UNCOMMITED="false"
+git diff-index --quiet HEAD -- || GIT_UNCOMMITED="true"
+
+
 curl -o voipdp-server_deb_amd64.tgz \
      http://download.komendant.pro/voip-dp/linux/voipdp-server_$PLATFORM.tgz
 if ! sha1sum -c voipdp-server_${VERSION}_${PLATFORM}.sha1; then
@@ -9,5 +22,11 @@ if ! sha1sum -c voipdp-server_${VERSION}_${PLATFORM}.sha1; then
     exit 1
 fi
 tar --extract -f voipdp-server_deb_amd64.tgz voipdp-server+$PLATFORM.deb
-docker build -t mmvrus/voipdp-server:$(VERSION)-debian8 .
+docker build -t $DOCKER_HUB_LOGIN/voipdp-server:$VERSION-debian8 \
+        --build-arg "GIT_ORIGIN=$GIT_ORIGIN" \
+        --build-arg "GIT_DESCRIBE=$GIT_DESCRIBE" \
+        --build-arg "GIT_BRANCH=$GIT_BRANCH" \
+        --build-arg "GIT_UNCOMMITED=$GIT_UNCOMMITED" \
+        $@ \
+        .
 popd
